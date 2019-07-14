86158dfd693da7901f4c01d5d77034d95517b796
Itorius <itorius.yt@gmail.com>
2019-07-14 22:03:01 +0200

    Show Intro Message only for my mods

--- Hooking/Hooking_IntroMessage.cs
+++ Hooking/Hooking_IntroMessage.cs
@@ -4,10 +4,12 @@ using MonoMod.Cil;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Terraria;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Core;
 using Terraria.UI;
 
 namespace BaseLibrary
@@ -29,7 +31,19 @@ namespace BaseLibrary
 				Dictionary<string, Version> previousVersions = new Dictionary<string, Version>();
 				if (File.Exists(LastVersionsPath)) previousVersions = JsonConvert.DeserializeObject<Dictionary<string, Version>>(File.ReadAllText(LastVersionsPath));
 
-				newOrUpdated = ModLoader.Mods.Where(mod => previousVersions.ContainsKey(mod.Name) && previousVersions[mod.Name] != mod.Version || !previousVersions.ContainsKey(mod.Name)).ToList();
+				newOrUpdated = ModLoader.Mods.Where(mod =>
+				{
+					Type type = typeof(ModLoader).Assembly.GetType("Terraria.ModLoader.Core.BuildProperties");
+					TmodFile file = mod.GetValue<TmodFile>("File");
+					if (file != null)
+					{
+						object properties = type.InvokeMethod<object>("ReadModFile", null, file);
+						string author = properties.GetValue<string>("author");
+						if (!author.Contains("Itorius")) return false;
+					}
+					
+					return previousVersions.ContainsKey(mod.Name) && previousVersions[mod.Name] != mod.Version || !previousVersions.ContainsKey(mod.Name);
+				}).ToList();
 				if (newOrUpdated.Count > 0) Main.menuMode = 4040;
 
 				File.WriteAllText(LastVersionsPath, JsonConvert.SerializeObject(ModLoader.Mods.Select(mod => new {Key = mod.Name, Value = mod.Version.ToString()}).ToDictionary(x => x.Key, x => x.Value)));
