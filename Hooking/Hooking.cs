f2214d2659fdfa81f9ae62ca0c7a66eeb9b59b99
Itorius <itorius.yt@gmail.com>
2019-07-23 10:54:24 +0200

    Close IHasUI items UIs on left click/drop

--- Hooking/Hooking.cs
+++ Hooking/Hooking.cs
@@ -1,15 +1,18 @@
-﻿using IL.Terraria;
+﻿using BaseLibrary.UI;
 using MonoMod.Cil;
 using MonoMod.RuntimeDetour.HookGen;
 using Newtonsoft.Json;
 using On.Terraria.UI;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
+using Terraria;
 using Terraria.ModLoader;
-using Terraria.ModLoader.Core;
+using Main = IL.Terraria.Main;
+using Player = On.Terraria.Player;
 
 namespace BaseLibrary
 {
@@ -19,6 +22,8 @@ namespace BaseLibrary
 
 		internal static void Load()
 		{
+			ItemSlot.LeftClick_ItemArray_int_int += ItemSlot_LeftClick;
+			Player.DropSelectedItem += Player_DropSelectedItem;
 			UIElement.GetElementAt += UIElement_GetElementAt;
 
 			Main.DrawInterface_36_Cursor += Main_DrawInterface_36_Cursor;
@@ -31,10 +36,9 @@ namespace BaseLibrary
 			Scheduler.EnqueueMessage(() => Terraria.Main.OnPostDraw += OnPostDraw);
 
 			Initialize();
-
 		}
 
-		private static void Hooking_do_worldGenCallBack_Hook(orig_do_worldGenCallBack orig)
+		private static void Hooking_do_worldGenCallBack_Hook(orig_do_worldGenCallBack orig, CancellationToken token)
 		{
 			Dictionary<string, Version> previousVersions = new Dictionary<string, Version>();
 			if (File.Exists(LastVersionsPath)) previousVersions = JsonConvert.DeserializeObject<Dictionary<string, Version>>(File.ReadAllText(LastVersionsPath));
@@ -45,27 +49,27 @@ namespace BaseLibrary
 			newOrUpdated = ModLoader.Mods.Where(mod =>
 			{
 				object o = arr.FirstOrDefault(x => x.GetValue<string>("Name") == mod.Name);
-				if (o != null)
-				{
-					if (!o.GetValue<object>("properties").GetValue<string>("author").Contains("Itorius")) return false;
-				}
+				if (o != null && !o.GetValue<object>("properties").GetValue<string>("author").Contains("Itorius")) return false;
 
 				return previousVersions.ContainsKey(mod.Name) && previousVersions[mod.Name] != mod.Version || !previousVersions.ContainsKey(mod.Name);
 			}).ToList();
 			if (newOrUpdated.Count > 0) Terraria.Main.menuMode = 4040;
 
-			File.WriteAllText(LastVersionsPath, JsonConvert.SerializeObject(ModLoader.Mods.Select(mod => new { Key = mod.Name, Value = mod.Version.ToString() }).ToDictionary(x => x.Key, x => x.Value)));
+			File.WriteAllText(LastVersionsPath, JsonConvert.SerializeObject(ModLoader.Mods.Select(mod => new {Key = mod.Name, Value = mod.Version.ToString()}).ToDictionary(x => x.Key, x => x.Value)));
 
-			orig();
+			orig(token);
 		}
 
-		public delegate void orig_do_worldGenCallBack();
-		public delegate void hook_do_worldGenCallBack(orig_do_worldGenCallBack orig);
+		public delegate void orig_do_worldGenCallBack(CancellationToken token);
+
+		public delegate void hook_do_worldGenCallBack(orig_do_worldGenCallBack orig, CancellationToken token);
+
+		public static MethodBase method => typeof(ModLoader).Assembly.GetType("Terraria.ModLoader.ModContent").GetMethod("SetupRecipes", Utility.defaultFlags, null, new[] {typeof(CancellationToken)}, null);
 
 		public static event hook_do_worldGenCallBack do_worldGenCallBack_Hook
 		{
-			add => HookEndpointManager.Add(typeof(ModLoader).Assembly.GetType("Terraria.ModLoader.ModContent").GetMethod("SetupRecipes",Utility.defaultFlags), value);
-			remove => HookEndpointManager.Remove(typeof(ModLoader).Assembly.GetType("Terraria.ModLoader.ModContent").GetMethod("SetupRecipes", Utility.defaultFlags), value);
+			add => HookEndpointManager.Add(method, value);
+			remove => HookEndpointManager.Remove(method, value);
 		}
 
 		internal static void Unload()
@@ -73,6 +77,20 @@ namespace BaseLibrary
 			Uninitialize();
 
 			Scheduler.EnqueueMessage(() => Terraria.Main.OnPostDraw -= OnPostDraw);
+		}
+
+		private static void ItemSlot_LeftClick(ItemSlot.orig_LeftClick_ItemArray_int_int orig, Item[] inv, int context, int slot)
+		{
+			if (inv[slot].modItem is IHasUI hasUI) BaseLibrary.PanelGUI.UI.CloseUI(hasUI);
+
+			orig(inv, context, slot);
+		}
+
+		private static void Player_DropSelectedItem(Player.orig_DropSelectedItem orig, Terraria.Player self)
+		{
+			if (self.HeldItem.modItem is IHasUI hasUI) BaseLibrary.PanelGUI.UI.CloseUI(hasUI);
+
+			orig(self);
 		}
 	}
 }
