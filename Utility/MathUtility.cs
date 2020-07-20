a14a0d3f693d7106e61e7a5930ff87586416d67a
Itorius <itorius.yt@gmail.com>
2020-07-20 16:46:34 +0200

    MathUtility

---
+++ Utility/MathUtility.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace BaseLibrary.Utility
+{
+	public static class MathUtility
+	{
+		public static T Min<T>(params T[] array) => array.Min();
+	}
+}
--- Utility/ReflectionUtility.cs
+++ Utility/ReflectionUtility.cs
@@ -1,7 +1,18 @@
-﻿namespace BaseLibrary.Utility
+﻿using System;
+using System.Reflection;
+
+namespace BaseLibrary.Utility
 {
-	public static class Utility
+	public static class ReflectionUtility
 	{
-		
+		public const BindingFlags DefaultFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+
+		public static void SetValue(this Type type, string name, object value, object obj = null, BindingFlags flags = DefaultFlags)
+		{
+			if (type.GetProperty(name, flags) != null) type.GetProperty(name, flags)?.SetValue(obj, value);
+			else type.GetField(name, flags)?.SetValue(obj, value);
+		}
+
+		public static void SetValue(this object obj, string name, object value, BindingFlags flags = DefaultFlags) => obj.GetType().SetValue(name, value, obj, flags);
 	}
 }
