978b0fbf255c1097e99823afe302aae0da4ac0a9
Itorius <itorius.yt@gmail.com>
2020-07-20 16:46:27 +0200

    BaseItem

---
+++ BaseItem.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ModLoader;
+
+namespace BaseLibrary
+{
+	public abstract class BaseItem : ModItem
+	{
+		public override string Texture => "BaseLibrary/Assets/PlaceholderTexture";
+	}
+}
