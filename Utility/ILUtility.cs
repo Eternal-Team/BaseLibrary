27a5efa1d299384db66aaab474b9850910707441
Itorius <itorius.yt@gmail.com>
2020-11-14 13:40:00 +0100

    ILUtility.AddVariable

---
+++ Utility/ILUtility.cs
@@ -0,0 +1,14 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+namespace BaseLibrary.Utility
+{
+	public static class ILUtility
+	{
+		public static int AddVariable<T>(this ILContext context)
+		{
+			context.Body.Variables.Add(new VariableDefinition(context.Import(typeof(T))));
+			return context.Body.Variables.Count - 1;
+		}
+	}
+}
