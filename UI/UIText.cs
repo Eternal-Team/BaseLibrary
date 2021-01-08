41ef80e11d4edda23c2bc9d0c80a5074c3844a70
Itorius <itorius.yt@gmail.com>
2021-01-08 13:32:52 +0100

    UIText settings

--- UI/UIText.cs
+++ UI/UIText.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using ReLogic.Content;
 using ReLogic.Graphics;
 using Terraria;
 using Terraria.GameContent;
@@ -23,8 +22,30 @@ namespace BaseLibrary.UI
 		Bottom
 	}
 
+	public struct UITextOptions
+	{
+		public static readonly UITextOptions Default = new UITextOptions
+		{
+			TextColor = Color.White,
+			BorderColor = Color.Black,
+			HorizontalAlignment = HorizontalAlignment.Left,
+			VerticalAlignment = VerticalAlignment.Top,
+			ScaleToFit = false,
+			Font = FontAssets.MouseText.Value
+		};
+
+		public Color TextColor;
+		public Color BorderColor;
+		public HorizontalAlignment HorizontalAlignment;
+		public VerticalAlignment VerticalAlignment;
+		public bool ScaleToFit;
+		public DynamicSpriteFont Font;
+	}
+
 	public class UIText : BaseElement
 	{
+		public UITextOptions Settings = UITextOptions.Default;
+
 		public object Text
 		{
 			get => text;
@@ -35,39 +56,30 @@ namespace BaseLibrary.UI
 			}
 		}
 
-		public Color TextColor = Color.White;
-		public Color BorderColor = Color.Black;
-
-		public HorizontalAlignment HorizontalAlignment = HorizontalAlignment.Left;
-		public VerticalAlignment VerticalAlignment = VerticalAlignment.Top;
-
-		public bool ScaleToFit;
-
 		private object text;
 
 		private float textScale;
-		private Asset<DynamicSpriteFont> font;
 		private Vector2 textSize;
 		private Vector2 textPosition;
 
 		public UIText(string text, float scale = 1f)
 		{
 			this.text = text;
-			font = scale > 1f ? FontAssets.DeathText : FontAssets.MouseText;
+			Settings.Font = scale > 1f ? FontAssets.DeathText.Value : FontAssets.MouseText.Value;
 			textScale = scale > 1f ? scale * 0.5f : scale;
 		}
 
 		public UIText(LocalizedText text, float scale = 1f)
 		{
 			this.text = text;
-			font = scale > 1f ?FontAssets.DeathText : FontAssets.MouseText;
+			Settings.Font = scale > 1f ? FontAssets.DeathText.Value : FontAssets.MouseText.Value;
 			textScale = scale > 1f ? scale * 0.5f : scale;
 		}
 
 		public UIText(Ref<string> text, float scale = 1f)
 		{
 			this.text = text;
-			font = scale > 1f ?FontAssets.DeathText : FontAssets.MouseText;
+			Settings.Font = scale > 1f ? FontAssets.DeathText.Value : FontAssets.MouseText.Value;
 			textScale = scale > 1f ? scale * 0.5f : scale;
 		}
 
@@ -81,16 +93,16 @@ namespace BaseLibrary.UI
 		protected override void Draw(SpriteBatch spriteBatch)
 		{
 			RasterizerState rasterizer = new RasterizerState { CullMode = CullMode.None, ScissorTestEnable = true };
-			
+
 			spriteBatch.End();
 
 			SamplerState samplerText = SamplerState.LinearClamp;
 			spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, samplerText, DepthStencilState.None, rasterizer, null, Main.UIScaleMatrix);
-			
-			Utils.DrawBorderStringFourWay(spriteBatch, font.Value, text.ToString(), textPosition.X, textPosition.Y, TextColor, BorderColor, Vector2.Zero, textScale);
-			
+
+			Utils.DrawBorderStringFourWay(spriteBatch, Settings.Font, text.ToString(), textPosition.X, textPosition.Y, Settings.TextColor, Settings.BorderColor, Vector2.Zero, textScale);
+
 			spriteBatch.End();
-			
+
 			SamplerState sampler = SamplerState.PointClamp;
 			spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, sampler, DepthStencilState.None, rasterizer, null, Main.UIScaleMatrix);
 		}
@@ -104,17 +116,20 @@ namespace BaseLibrary.UI
 				return;
 			}
 
-			textSize = font.Value.MeasureString(text.ToString());
-			if (ScaleToFit) textScale = Math.Min(InnerDimensions.Width / textSize.X, InnerDimensions.Height / textSize.Y);
+			textSize = Settings.Font.MeasureString(text.ToString());
+			if (Settings.ScaleToFit) textScale = Math.Min(InnerDimensions.Width / textSize.X, InnerDimensions.Height / textSize.Y);
 			textSize *= textScale;
 
-			if (HorizontalAlignment == HorizontalAlignment.Left) textPosition.X = InnerDimensions.X;
-			else if (HorizontalAlignment == HorizontalAlignment.Center) textPosition.X = InnerDimensions.X + InnerDimensions.Width * 0.5f - textSize.X * 0.5f;
-			else if (HorizontalAlignment == HorizontalAlignment.Right) textPosition.X = InnerDimensions.X + InnerDimensions.Width - textSize.X;
+			var hAlign = Settings.HorizontalAlignment;
+			var vAlign = Settings.VerticalAlignment;
 
-			if (VerticalAlignment == VerticalAlignment.Top) textPosition.Y = InnerDimensions.Y;
-			else if (VerticalAlignment == VerticalAlignment.Center) textPosition.Y = InnerDimensions.Y + InnerDimensions.Height * 0.5f - textSize.Y * 0.5f + 8f * textScale;
-			else if (VerticalAlignment == VerticalAlignment.Bottom) textPosition.Y = InnerDimensions.Y + InnerDimensions.Height - textSize.Y + 8f * textScale;
+			if (hAlign == HorizontalAlignment.Left) textPosition.X = InnerDimensions.X;
+			else if (hAlign == HorizontalAlignment.Center) textPosition.X = InnerDimensions.X + InnerDimensions.Width * 0.5f - textSize.X * 0.5f;
+			else if (hAlign == HorizontalAlignment.Right) textPosition.X = InnerDimensions.X + InnerDimensions.Width - textSize.X;
+
+			if (vAlign == VerticalAlignment.Top) textPosition.Y = InnerDimensions.Y;
+			else if (vAlign == VerticalAlignment.Center) textPosition.Y = InnerDimensions.Y + InnerDimensions.Height * 0.5f - textSize.Y * 0.5f + 8f * textScale;
+			else if (vAlign == VerticalAlignment.Bottom) textPosition.Y = InnerDimensions.Y + InnerDimensions.Height - textSize.Y + 8f * textScale;
 		}
 	}
 }
