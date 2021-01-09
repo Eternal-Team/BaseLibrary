879f6158232448e128a4a29bdb199a6e0510d027
Itorius <itorius.yt@gmail.com>
2021-01-09 17:49:48 +0100

    Custom cursor

--- Hooking.cs
+++ Hooking.cs
@@ -6,6 +6,7 @@
 		{
 			IL.Terraria.Recipe.FindRecipes += Recipe_FindRecipes;
 			IL.Terraria.Recipe.Create += Recipe_Create;
+			IL.Terraria.Main.DrawInterface_36_Cursor += DrawCursor;
 
 			// if (ModLoader.GetMod("BaseLibrary") != null) ItemSlot.OverrideHover += ItemSlot_OverrideHover;
 		}
