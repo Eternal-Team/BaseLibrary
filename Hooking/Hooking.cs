7f27615173ad8e85c7f6dc540bcbb32ad4276a99
Itorius <itorius.yt@gmail.com>
2022-05-18 20:11:36 +0200

    Mouse Item Storage UI logic out of BaseLibrary

--- Hooking/Hooking.cs
+++ Hooking/Hooking.cs
@@ -7,13 +7,9 @@ internal static partial class Hooking
 {
 	internal static void Initialize()
 	{
-		// Recipe.FindRecipes += Recipe_FindRecipes;
-		// Recipe.Create += Recipe_Create;
 		Main.DrawInterface_36_Cursor += DrawCursor;
 
 		Input.Load();
 		PlayerInput.UpdateInput += PlayerInputOnUpdateInput;
-
-		// ItemSlot.OverrideHover_ItemArray_int_int += ItemSlot_OverrideHover;
 	}
 }
