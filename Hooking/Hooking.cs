9b62e03136cee762ae15cd00b3625c932d4f2fae
Itorius <itorius.yt@gmail.com>
2022-08-28 21:55:10 +0200

    Don't play default sound for IHasUI items

--- Hooking/Hooking.cs
+++ Hooking/Hooking.cs
@@ -1,5 +1,14 @@
-﻿using On.Terraria.GameContent.UI.Elements;
+﻿using System;
+using System.Reflection;
+using BaseLibrary.UI;
+using BaseLibrary.Utility;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour.HookGen;
+using On.Terraria.GameContent.UI.Elements;
 using On.Terraria.GameInput;
+using Terraria;
+using Terraria.ModLoader;
 
 namespace BaseLibrary;
 
@@ -10,5 +19,27 @@ internal static partial class Hooking
 		Input.Load();
 		PlayerInput.UpdateInput += PlayerInputOnUpdateInput;
 		UIKeybindingListItem.OnClickMethod += UIKeybindingListItemOnOnClickMethod;
+
+		MethodInfo methodInfo = typeof(ItemLoader).GetMethod("RightClick", ReflectionUtility.DefaultFlags_Static);
+		HookEndpointManager.Modify(methodInfo, ItemLoaderRightClick);
+	}
+
+	private static void ItemLoaderRightClick(ILContext il)
+	{
+		ILCursor cursor = new ILCursor(il);
+		ILLabel label = cursor.DefineLabel();
+
+		if (!cursor.TryGotoNext(MoveType.AfterLabel, i => i.MatchLdcI4(7), i => i.MatchLdcI4(-1), i => i.MatchLdcI4(-1)))
+			throw new Exception("IL edit failed");
+
+		cursor.Emit(OpCodes.Ldarg, 0);
+		cursor.EmitDelegate((Item item) => item.ModItem is not IHasUI);
+		cursor.Emit(OpCodes.Brfalse, label);
+
+		if (!cursor.TryGotoNext(MoveType.AfterLabel, i => i.MatchPop()))
+			throw new Exception("IL edit failed");
+
+		cursor.Index++;
+		cursor.MarkLabel(label);
 	}
 }
