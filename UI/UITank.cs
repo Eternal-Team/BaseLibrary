f7fb1b84dcc8935cf1f15422926e08ec4be0d119
Itorius <itorius.yt@gmail.com>
2022-08-29 14:08:40 +0200

    UITank mouse interaction, mouse text and bug fix

--- UI/UITank.cs
+++ UI/UITank.cs
@@ -8,24 +8,56 @@ using Terraria.ModLoader;
 
 namespace BaseLibrary.UI;
 
+public struct UITankSettings
+{
+	public static readonly UITankSettings Default = new()
+	{
+		AllowMouseInteraction = true
+	};
+
+	public bool AllowMouseInteraction;
+}
+
 [ExtendsFromMod("FluidLibrary")]
 public class UITank : BaseElement
 {
 	private FluidStorage storage;
-	private int slot;
+	private int tank;
 
-	public FluidStack FluidStack => storage[slot];
+	public UITankSettings Settings = UITankSettings.Default;
 
-	public UITank(FluidStorage itemStorage, int slot)
+	public FluidStack FluidStack => storage[tank];
+
+	public UITank(FluidStorage itemStorage, int tank)
 	{
 		Width.Pixels = 44;
 		Height.Pixels = 44;
 
-		this.slot = slot;
+		this.tank = tank;
 		storage = itemStorage;
 	}
 
-	public override int CompareTo(BaseElement other) => other is UITank uiSlot ? slot.CompareTo(uiSlot.slot) : 0;
+	protected override void MouseDown(MouseButtonEventArgs args)
+	{
+		if (!Settings.AllowMouseInteraction && args.Button != MouseButton.Left && args.Button != MouseButton.Right)
+			return;
+
+		args.Handled = true;
+
+		if (Main.mouseItem.IsAir || Main.mouseItem.ModItem is not IFluidStorage mouseStorage)
+			return;
+
+		if (args.Button == MouseButton.Left)
+		{
+			storage.TransferTo(Main.LocalPlayer, mouseStorage.GetFluidStorage(), tank);
+		}
+		else if (args.Button == MouseButton.Right)
+		{
+			mouseStorage.GetFluidStorage().TransferTo(Main.LocalPlayer, storage, 0 /* todo: it should try all tanks*/);
+		}
+	}
+
+	public override int CompareTo(BaseElement other) => other is UITank uiSlot ? tank.CompareTo(uiSlot.tank) : 0;
 
 	protected override void Draw(SpriteBatch spriteBatch)
 	{
@@ -68,24 +100,38 @@ public class UITank : BaseElement
 		DrawingUtility.DrawSlot(spriteBatch, Dimensions, texture, Color.White);
 		spriteBatch.End();
 
-		if (FluidStack.Fluid == null)
-			return;
+		if (FluidStack.Fluid is not null)
+		{
+			spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearWrap, stencilLiquid, rasterizerState, null, Main.UIScaleMatrix);
 
-		Texture2D fluidTexture = ModContent.Request<Texture2D>(FluidStack.Fluid.Texture).Value;
+			Texture2D fluidTexture = ModContent.Request<Texture2D>(FluidStack.Fluid.Texture).Value;
 
-		float fill = FluidStack.Volume / (float)storage.MaxVolumeFor(slot);
+			float fill = FluidStack.Volume / (float)storage.MaxVolumeFor(tank);
 
-		spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearWrap, stencilLiquid, rasterizerState, null, Main.UIScaleMatrix);
+			// Draw fluid (use stencil)
+			int height = (int)(Dimensions.Height * fill);
+			spriteBatch.Draw(fluidTexture, new Rectangle((int)Dimensions.BottomLeft().X, (int)Dimensions.BottomLeft().Y - height, Dimensions.Width, height), Color.White);
 
-		// Draw fluid (use stencil)
-		int height = (int)(Dimensions.Height * fill);
-		spriteBatch.Draw(fluidTexture, new Rectangle((int)Dimensions.BottomLeft().X, (int)Dimensions.BottomLeft().Y - height, Dimensions.Width, height), Color.White);
-
-		spriteBatch.End();
+			spriteBatch.End();
+		}
 
 		spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.None, rasterizerState, shader, Main.UIScaleMatrix);
 
 		// Draw border of slot
 		DrawingUtility.DrawSlot(spriteBatch, Dimensions, ModContent.Request<Texture2D>(BaseLibrary.AssetPath + "Textures/UI/SlotBorder").Value, Color.White);
+
+		if (IsMouseHovering)
+		{
+			// todo: draw BG
+			
+			if (FluidStack.Fluid is not null)
+				Main.instance.MouseText($"Storing {FluidStack.Volume / 255f:0.#}/{storage.MaxVolumeFor(tank) / 255f:0.#} of {FluidStack.Fluid.DisplayName.Get()}\n" +
+				                        "Left-click with a fluid container to fill it\n" +
+				                        "Right-click with a fluid container to empty it");
+			else
+				Main.instance.MouseText("Empty\n" +
+				                        "Left-click with a fluid container to fill it\n" +
+				                        "Right-click with a fluid container to empty it");
+		}
 	}
 }
