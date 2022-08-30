b98eeb454a0a8b568d5626abb3dfc8c92f361aa7
Itorius <itorius.yt@gmail.com>
2022-08-30 20:50:34 +0200

    NetUtility RW Point16

--- Utility/NetUtility.cs
+++ Utility/NetUtility.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using Terraria.DataStructures;
 
 namespace BaseLibrary.Utility;
 
@@ -15,5 +16,16 @@ public static class NetUtility
 	public static Guid ReadGuid(this BinaryReader reader)
 	{
 		return new Guid(reader.ReadBytes(reader.ReadInt32()));
+	}
+	
+	public static void Write(this BinaryWriter writer, Point16 id)
+	{
+		writer.Write(id.X);
+		writer.Write(id.Y);
+	}
+
+	public static Point16 ReadPoint16(this BinaryReader reader)
+	{
+		return new Point16(reader.ReadInt16(), reader.ReadInt16());
 	}
 }
