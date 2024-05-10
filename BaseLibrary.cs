4743f661373af3171cb237ba453676b65de17717
Itorius <itorius.yt@gmail.com>
2024-05-10 19:27:10 +0200

    Version 3

---
+++ BaseLibrary.cs
@@ -0,0 +1,13 @@
+using System;
+using Terraria.ModLoader;
+
+namespace BaseLibrary
+{
+    public class BaseLibrary : Mod
+    {
+        public override void Load()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
