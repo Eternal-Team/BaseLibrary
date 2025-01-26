a8340e5c6d97721dcbad40dcbaabc6b9e6f72ecc
itorius <itorius.yt@gmail.com>
2025-01-26 17:39:23 +0100

    Guid serializer

---
+++ Utility/GuidSerializer.cs
@@ -0,0 +1,24 @@
+using System;
+using Terraria.ModLoader.IO;
+
+namespace BaseLibrary;
+
+public class GUIDSerializer : TagSerializer<Guid, TagCompound>
+{
+	public override TagCompound Serialize(Guid value) => new TagCompound
+	{
+		["Value"] = value.ToString()
+	};
+
+	public override Guid Deserialize(TagCompound tag)
+	{
+		try
+		{
+			return Guid.Parse(tag.GetString("Value"));
+		}
+		catch
+		{
+			return Guid.NewGuid();
+		}
+	}
+}
